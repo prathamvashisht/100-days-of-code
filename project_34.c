00596d6568713a1a80a3f8328264c532a28ab2ee
prathamvashisht <pratham09082008@gmail.com>
2026-08-26 21:28:42 +0530

    Update project_34.c

--- project_34.c
+++ project_34.c
@@ -1,4 +1,18 @@
+//Q34: Write a program to check if a number is prime.
 
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Prime
+
+Input 2:
+10
+Output 2:
+Not prime
+
+*/
 #include <stdio.h>
 
 int main()
