cc66905d2abf2457a74fe9d6277421fe9e7f0665
prathamvashisht <pratham09082008@gmail.com>
2026-09-03 23:00:33 +0530

    Add files via upload

---
+++ project_50.c
@@ -0,0 +1,37 @@
+// Q50: Write a program to print the following pattern:
+//*****
+//****
+//***
+//**
+//*
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 5; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
