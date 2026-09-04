72a980b4302ea5b0096de549b5d590dfe7f747e3
prathamvashisht <pratham09082008@gmail.com>
2026-09-04 11:35:42 +0530

    Add files via upload

---
+++ project_51.c
@@ -0,0 +1,43 @@
+/*Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345*\
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+
+        for (int space = 1; space <= i - 1; space++)
+        {
+            printf(" ");
+        }
+
+        for (int j = i; j <= n; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
